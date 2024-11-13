ac28597c3f9e005836e0c5b10906b1f6a2705941
Arthur Silva Dantas <arthur.dantas@ufms.br>
2024-11-13 00:21:09 -0300

    Add metodos getters e setters

--- src/Personagem.java
+++ src/Personagem.java
@@ -3,12 +3,54 @@ public abstract class Personagem{
     private double saude;
     private double forca;
     private double destreza;
+    private Arma arma;
 
-    public Personagem(String nomeTipo, double saude, double forca, double destreza){
+    public Personagem(String nomeTipo, double saude, double forca, double destreza, Arma arma){
         this.nomeTipo = nomeTipo;
         this.saude = saude;
         this.forca = forca;
         this.destreza = destreza;
+        this.arma = arma;
+    }
+
+    public String getNomeTipo(){
+        return this.nomeTipo;
+    }
+
+    public double getSaude(){
+        return this.saude;
+    }
+
+    public double getForca(){
+        return this.forca;
+    }
+
+    public double getDestreza(){
+        return this.destreza;
+    }
+
+    public Arma getArma(){
+        return this.arma;
+    }
+
+    public void setNomeTipo(String nomeTipo){
+        this.nomeTipo = nomeTipo;
+    }
+
+    public void setSaude(double saude){
+        this.saude = saude;
+    }
+
+    public void setForca(double forca){
+        this.forca = forca;
+    }
+
+    public void setDestreza(double destreza){
+        this.destreza = destreza;
+    }
+
+    public void setArma(Arma arma){
+        this.arma = arma;
     }
 
     public void printStatus(){
@@ -29,4 +71,4 @@ public abstract class Personagem{
     private boolean estaMorto() {
         return this.saude < 1;
     }
-} 
+}
